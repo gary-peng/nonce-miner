da6758dc98dd93fd6a1a42b38e8b3a9c49deaa86
Gary Peng <49541146+gary-peng@users.noreply.github.com>
2023-04-14 05:17:57 -0400

    Multithreading

--- mine.cpp
+++ mine.cpp
@@ -1,8 +1,12 @@
 #include <mpi.h>
 #include <cstdio>
 #include <iostream>
+#include <thread>
 #include "Keccak256.hpp"
 #include "uint256_t.h"
+
+#define DIFF 22
+#define LEFT_OFF 0
 
 void printArr(std::uint8_t* arr, size_t size, std::string label) {
     std::cout << label + ": ";
@@ -12,35 +16,11 @@ void printArr(std::uint8_t* arr, size_t size, std::string label) {
     printf("\n");
 }
 
-int main(int argc, char *argv[]) {
-	MPI_Init(&argc, &argv);
-	MPI_Comm comm = MPI_COMM_WORLD;
-	int p, rank;
-	MPI_Comm_size(comm, &p);
-	MPI_Comm_rank(comm, &rank);
+void hash(uint256_t threadStartNonce, uint256_t threadEndNonce, int rank, int threadRank, uint256_t address, uint256_t mask, double startTime) {
+    uint256_t nonce = threadStartNonce;
 
-    int d = 33;
-    uint256_t mask = (uint256_t) 1 << d;
-
-    // 0xe7e9033363B988d46fEa4cA7d80ecFc1215eD436
-    uint256_t address = ((uint256_t) 0xe7e9033363B988d4 << 96) | ((uint256_t) 0x6fEa4cA7d80ecFc1 << 32) | ((uint256_t) 0x215eD436);
-    // std::cout << address << std::endl;
-
-    uint256_t load = mask / (uint256_t) p;
-    uint256_t leftOff = 9400000;
-    uint256_t nonce = rank * load;
-    uint256_t startNonce = nonce;
-    nonce += leftOff;
-    uint256_t endNonce;
-    if (rank == p-1) {
-        endNonce = mask;
-    } else {
-        endNonce = nonce + load;
-    }
-    std::cout << "Proc " << rank << ": " << nonce << "-" << endNonce << std::endl;
-
-    while (nonce < endNonce) {
-        if (rank == 0) std::cout << "Work: " << nonce - startNonce << std::endl;
+    while (nonce < threadEndNonce) {
+        if (rank == 0 && threadRank == 0) std::cout << "Work: " << nonce - threadStartNonce << std::endl;
     
         std::uint8_t data[64] = {0};
         for (size_t i=0; i<32; i++) {
@@ -69,12 +49,63 @@ int main(int argc, char *argv[]) {
         if (val == 0) {
             printArr(data, 64, "data");
             printArr(hash, 32, "hash");
-            std::cout << val << std::endl;
-            MPI_Abort(comm, MPI_SUCCESS);
+            std::cout << rank << "nonce: " << nonce << std::endl;
+
+            double totalTime = MPI_Wtime()-startTime;
+            printf("Time: %.6f\n", totalTime);
+
+            MPI_Abort(MPI_COMM_WORLD, MPI_SUCCESS);
             break;
         }
 
         nonce++;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int NUM_THREADS = atoi(argv[1]);
+    // std::cout << "num threads: " << NUM_THREADS << std::endl;
+
+    int buf[100], provided;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided); 
+	MPI_Comm comm = MPI_COMM_WORLD;
+	int p, rank;
+	MPI_Comm_size(comm, &p);
+	MPI_Comm_rank(comm, &rank);
+
+    uint256_t mask = (uint256_t) 1 << DIFF;
+    // 0xe7e9033363B988d46fEa4cA7d80ecFc1215eD436
+    uint256_t address = ((uint256_t) 0xe7e9033363B988d4 << 96) | ((uint256_t) 0x6fEa4cA7d80ecFc1 << 32) | ((uint256_t) 0x215eD436);
+
+    uint256_t searchSpace = (uint256_t) 1 << 32;
+    uint256_t load = searchSpace / (uint256_t) p;
+    uint256_t leftOff = 0;
+    uint256_t startNonce = rank * load;
+    uint256_t endNonce;
+    if (rank == p-1) {
+        endNonce = searchSpace;
+    } else {
+        endNonce = startNonce + load;
+    }
+    std::cout << "Proc " << rank << ": " << startNonce << "-" << endNonce << std::endl;
+
+    std::thread thrds[NUM_THREADS];
+    for (int i=0; i<NUM_THREADS; i++){
+        uint256_t threadLoad = load / NUM_THREADS;
+        uint256_t threadStartNonce = (threadLoad * i) + startNonce;
+        uint256_t threadEndNonce;
+        if (i == NUM_THREADS-1) {
+            threadEndNonce = endNonce;
+        } else {
+            threadEndNonce = threadStartNonce + threadLoad;
+        }
+
+        std::cout << "Proc " << rank << "|" << i << ": " << threadStartNonce << "-" << threadEndNonce << std::endl;
+
+        thrds[i] = std::thread(hash, threadStartNonce, threadEndNonce, rank, i, address, mask, MPI_Wtime());
+    }
+    for (size_t i=0; i<NUM_THREADS; i++){
+        thrds[i].join();
     }
 
 
@@ -85,4 +116,5 @@ int main(int argc, char *argv[]) {
 /*
 mpicxx -o bin/mine *.cpp
 mpirun -np 4 bin/mine
+85577470
 */

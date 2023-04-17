ac75844644ed12b5780dab4fdd2c781a2585b7f4
Gary Peng <49541146+gary-peng@users.noreply.github.com>
2023-04-17 02:14:24 -0400

    Optimized

--- mine.cpp
+++ mine.cpp
@@ -1,14 +1,12 @@
 #include <mpi.h>
 #include <cstdio>
 #include <iostream>
-#include <thread>
 #include "Keccak256.hpp"
-#include "uint256_t.h"
+#include "Uint256.hpp"
 
-#define DIFF 33
-#define LEFT_OFF 0
+#define DIFF 42
 
-void printArr(std::uint8_t* arr, size_t size, std::string label) {
+void printArr(uint8_t* arr, size_t size, std::string label) {
     std::cout << label + ": ";
     for (size_t i=0; i<size; i++) {
         printf("%02x", arr[i]);
@@ -16,111 +14,112 @@ void printArr(std::uint8_t* arr, size_t size, std::string label) {
     printf("\n");
 }
 
-void hash(uint256_t threadStartNonce, uint256_t threadEndNonce, int rank, int threadRank, uint256_t address, uint256_t mask, double startTime) {
-    uint256_t nonce = threadStartNonce;
-
-    while (nonce < threadEndNonce) {
-        uint256_t work = nonce - threadStartNonce;
-        if (rank == 0 && threadRank == 0 && work%50000 == 0) std::cout << "Work: " << nonce - threadStartNonce << std::endl;
-    
-        std::uint8_t data[64] = {0};
-        for (size_t i=0; i<32; i++) {
-            int shift = (32-i-1)*8;
-            data[i] = nonce >> shift;
-        }
-        for (size_t i=32; i<64; i++) {
-            int shift = (64-i-1)*8;
-            data[i] = address >> shift;
-        }
-        // printArr(data, 64, "data");
-        
-        std::uint8_t hash[Keccak256::HASH_LEN];
-        Keccak256::getHash(data, 64, hash);
-        // printArr(hash, 32, "hash");
-
-        uint256_t hashInt = 0;
-        for (int i=31; i>=0; i--) {
-            int shift = (32-i-1)*8;
-            hashInt = hashInt | ((uint256_t) hash[i] << shift);
-        }
-        // std::cout << hashInt << std::endl;
-        uint256_t val = hashInt % mask;
-        // std::cout << val << std::endl;
-
-        if (val == 0) {
-            printArr(data, 64, "data");
-            printArr(hash, 32, "hash");
-            std::cout << rank << "nonce: " << nonce << std::endl;
-
-            double totalTime = MPI_Wtime()-startTime;
-            printf("Time: %.6f\n", totalTime);
-
-            MPI_Abort(MPI_COMM_WORLD, MPI_SUCCESS);
-            break;
-        }
-
-        nonce++;
+void printVal(uint32_t arr[Uint256::NUM_WORDS], std::string label) {
+    std::cout << label + ": ";
+    for (size_t i=0; i<Uint256::NUM_WORDS; i++) {
+        printf("%08x", arr[i]);
     }
+    printf("\n");
+}
+
+bool check(uint8_t hash[Keccak256::HASH_LEN], Uint256 &mask)
+{
+    Uint256 hashVal = Uint256(hash);
+    for (size_t i=0; i<Uint256::NUM_WORDS; i++) {
+        hashVal.value[i] &= mask.value[i];
+    }
+
+    if (hashVal == Uint256::ZERO) {
+        return true;
+    }
+    return false;
 }
 
 int main(int argc, char *argv[]) {
-    int NUM_THREADS = atoi(argv[1]);
-    // std::cout << "num threads: " << NUM_THREADS << std::endl;
-
-    int buf[100], provided;
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided); 
+    MPI_Init(&argc, &argv);
 	MPI_Comm comm = MPI_COMM_WORLD;
 	int p, rank;
 	MPI_Comm_size(comm, &p);
 	MPI_Comm_rank(comm, &rank);
 
-    uint256_t mask = (uint256_t) 1 << DIFF;
-    // 0xe7e9033363B988d46fEa4cA7d80ecFc1215eD436
-    uint256_t address = ((uint256_t) 0xe7e9033363B988d4 << 96) | ((uint256_t) 0x6fEa4cA7d80ecFc1 << 32) | ((uint256_t) 0x215eD436);
-
-    uint256_t searchEnd = ((uint256_t) 1 << 256) - 1;
-    uint256_t searchStart = 0;
-    uint256_t load = (searchEnd-searchStart) / (uint256_t) p;
-    uint256_t startNonce = (rank * load) + searchStart;
-    uint256_t endNonce;
-    if (rank == p-1) {
-        endNonce = searchEnd;
-    } else {
-        endNonce = startNonce + load;
+    uint64_t count = 0;
+    Uint256 stepSize = Uint256(); 
+    for (int i=0; i<p; i++) {
+        stepSize.add(Uint256::ONE);
     }
-    std::cout << "Proc " << rank << ": " << startNonce << "-" << endNonce << std::endl;
+    Uint256 leftOff = Uint256("0000000000000000000000000000000000000000000000000000000002BDE780");
 
-    double startTime = MPI_Wtime();
+    Uint256 address = Uint256("000000000000000000000000e7e9033363B988d46fEa4cA7d80ecFc1215eD436");
+    uint8_t addressArr[32];
+    address.getBigEndianBytes(addressArr);
+    uint8_t data[64] = {0};  
+    for (size_t i=0; i<32; i++) {
+        data[32+i] = addressArr[i];
+    }
+    uint8_t hash[Keccak256::HASH_LEN];
 
-    std::thread thrds[NUM_THREADS];
-    for (int i=0; i<NUM_THREADS; i++){
-        uint256_t threadLoad = load / NUM_THREADS;
-        uint256_t threadStartNonce = (threadLoad * i) + startNonce + LEFT_OFF;
-        uint256_t threadEndNonce;
-        if (i == NUM_THREADS-1) {
-            threadEndNonce = endNonce;
-        } else {
-            threadEndNonce = threadStartNonce + threadLoad;
+    Uint256 nonce = Uint256(); 
+    for (int i=0; i<rank; i++) {
+        nonce.add(Uint256::ONE);
+    }
+    nonce.add(leftOff);
+    uint8_t nonceArr[32];   
+    nonce.getBigEndianBytes(nonceArr);  
+
+    Uint256 mask = Uint256(Uint256::ONE);
+    for (size_t i=0; i<DIFF; i++) {
+        mask.shiftLeft1();
+    }
+    mask.subtract(Uint256::ONE);
+
+    if (rank == 0) {
+        std::cout << "================================================================================" << std::endl;
+        std::cout << "difficulty: " << std::to_string(DIFF) << std::endl;
+        printArr(data, 64, "data");
+        printf("step size: %d\n", stepSize.value[0]);
+        std::cout << "================================================================================" << std::endl;
+    }
+    printArr(nonceArr, 32, std::to_string(rank) + " starting nonce");
+
+    while (true) {
+        nonce.getBigEndianBytes(nonceArr);
+        for (size_t i=0; i<32; i++) {
+            data[i] = nonceArr[i];
         }
 
-        // std::cout << "Proc " << rank << "|" << i << ": " << threadStartNonce << "-" << threadEndNonce << std::endl;
+        if (rank == 0 && count%100000 == 0) {
+            std::cout << "work: " << count << std::endl;
+            printArr(nonceArr, 32, std::to_string(rank) + " nonce");
+        }
 
-        thrds[i] = std::thread(hash, threadStartNonce, threadEndNonce, rank, i, address, mask, startTime);
+        // if (count < 3) {
+        //     printArr(nonceArr, 32, std::to_string(rank) + " nonce");
+        // }
+
+        Keccak256::getHash(data, 64, hash);
+
+        if (check(hash, mask)) {
+            std::cout << "================================================================================" << std::endl;
+            printArr(data, 64, "DATA");
+            printArr(hash, Keccak256::HASH_LEN, "HASH");
+            printArr(nonceArr, 32, std::to_string(rank) + " FOUND NONCE");
+            std::cout << "================================================================================" << std::endl;
+            break;
+        }
+
+        count++;
+        nonce.add(stepSize);
     }
-    for (size_t i=0; i<NUM_THREADS; i++){
-        thrds[i].join();
-    }
 
-    double totalTime = MPI_Wtime()-startTime;
-    printf("Time: %.6f\n", totalTime);
-
+    MPI_Abort(comm, MPI_SUCCESS);
 	MPI_Finalize();
 	return 0;
 }
 
 /*
 mpicxx -o bin/mine *.cpp
-mpirun -np 4 bin/mine 10
+mpirun -np 120 bin/mine
 mpicxx -o bin/mine *.cpp -pthread
-85577470
+
+46000000
 */
